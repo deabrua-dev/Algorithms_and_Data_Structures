245600c02da437c5c71202ffe4143cfcf02ea25b
deabrua-dev <deabrua.co@gmail.com>
2022-03-14 17:43:11 +0200

    Stack.kt ready

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,3 @@
-import structures.PriorityQueue
-
 fun main() {
-    val queue = PriorityQueue<String>()
-    queue.push("Test0", 0)
-    queue.push("Test1", 2)
-    queue.push("Test2", 0)
-    queue.push("Test3", 1)
-    queue.push("Test4", 3)
-    queue.push("Test5", 1)
-    queue.push("Test6", 3)
-    queue.push("Test7", 1)
-    queue.push("Test8", 0)
-    queue.push("Test9", 2)
-    queue.push("Test10", 6)
-
-
-    while (queue.length() != 0) {
-        println(queue.front())
-        queue.pop()
-    }
     println("Ready")
 }
--- src/main/kotlin/structures/Stack.kt
+++ src/main/kotlin/structures/Stack.kt
@@ -1,4 +1,35 @@
 package structures
 
-class Stack {
+class Stack<T> {
+    class Node<T> (
+        private var value: T?,
+        private var next: Node<T>? = null
+    ) {
+        fun data() = this.value
+        fun next() = this.next
+    }
+
+    private var head: Node<T>? = null
+    private var length: Int = 0
+
+    fun top() = this.head?.data()
+
+    fun length() = this.length
+    fun isEmpty() = this.head == null
+
+    fun push(value: T) {
+        if (isEmpty()) {
+            this.head = Node(value)
+        } else {
+            val newNode = Node(value, this.head)
+            this.head = newNode
+        }
+        this.length++
+    }
+
+    fun pop() {
+        if (isEmpty()) return
+        this.head = this.head?.next()
+        this.length--
+    }
 }
