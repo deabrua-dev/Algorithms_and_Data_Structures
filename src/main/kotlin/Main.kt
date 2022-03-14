05b2e8c653c7716b3018a1f7516dfdc18cfd1e43
deabrua-dev <deabrua.co@gmail.com>
2022-03-14 11:09:52 +0200

    Queue.kt ready

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,16 @@
-import structures.DoublyLinkedList
+import structures.Queue
 
 fun main() {
-    val list = DoublyLinkedList<String>()
+    val queue = Queue<String>()
+    queue.push("Test0")
+    queue.push("Test1")
+    queue.push("Test2")
+    queue.push("Test3")
+    queue.push("Test4")
 
-    list.append("Test0")
-    list.append("Test1")
-    list.append("Test2")
-    list.append("Test3")
-    list.append("Test4")
-    list.append("Test5")
-    list.append("Test6")
-    list.insert(3,"Test2")
-    println("Test get: " + list[6])
-    list[6] = "Punk"
-    println("Test set: " + list[6])
-    list.remove(1)
-    println(list.indexOf("Test2"))
-    for(i in 0 until list.length()) {
-        println(list[i])
+    while (queue.length() != 0) {
+        println(queue.front())
+        queue.pop()
     }
+    println("Ready")
 }
--- src/main/kotlin/structures/Queue.kt
+++ src/main/kotlin/structures/Queue.kt
@@ -1,4 +1,47 @@
 package structures
 
-class Queue {
+class Queue<T> {
+    class Node<T> (
+        private var value: T?,
+        private var next: Node<T>? = null
+    ) {
+        fun data() = this.value
+        fun next() = this.next
+
+        fun changeData(value: T? = null) {
+            this.value = value
+        }
+
+        fun changeNext(next: Node<T>? = null) {
+            this.next = next
+        }
+    }
+
+    private var head: Node<T>? = null
+    private var tail: Node<T>? = null
+
+    private var length: Int = 0
+
+    fun front() = this.head?.data()
+    fun back() = this.tail?.data()
+
+    fun length() = this.length
+    fun isEmpty() = this.head == null
+
+    fun push(value: T) {
+        if (isEmpty()) {
+            this.head = Node(value)
+            this.tail = this.head
+        } else {
+            this.tail?.changeNext(Node(value))
+            this.tail = this.tail?.next()
+        }
+        this.length++
+    }
+
+    fun pop() {
+        if (isEmpty()) return
+        this.head = this.head?.next()
+        this.length--
+    }
 }
