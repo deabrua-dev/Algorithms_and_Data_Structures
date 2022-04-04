d6613dd29a54141c30205c6cd620e0609b179de4
deabrua-dev <deabrua.co@gmail.com>
2022-04-04 19:10:43 +0300

    HashTableOpenAddressingLinearProbing.kt ready

--- src/main/kotlin/structures/HashTableOpenAddressingLinearProbing.kt
+++ src/main/kotlin/structures/HashTableOpenAddressingLinearProbing.kt
@@ -1,4 +1,66 @@
 package structures
 
-class HashTableOpenAddressingLinearProbing {
+class HashTableOpenAddressingLinearProbing<T, R>(private var capacity: Int = 0) {
+    private data class Node<T, R>(val key: T, val value: R, var deleted: Boolean = false)
+
+    private var map = arrayOfNulls<Node<T, R>?>(this.capacity)
+    private var size = 0
+
+    fun push(key: T, value: R) {
+        var hashIndex = calculateHash(key)
+        var magicConstant = 1
+        while (
+            this.map[hashIndex] != null &&
+            this.map[hashIndex]?.key == key &&
+            this.map[hashIndex]?.deleted != true
+        ) {
+            hashIndex = (hashIndex + magicConstant) % this.capacity
+            magicConstant++
+        }
+        if (
+            this.map[hashIndex] == null &&
+            this.map[hashIndex]?.deleted != true
+        ) {
+            this.size++
+        }
+        this.map[hashIndex] = Node(key, value)
+    }
+
+    fun get(key: T) : R? {
+        var hashIndex = calculateHash(key)
+        var magicConstant = 1
+        while (this.map[hashIndex] != null) {
+            if (
+                this.map[hashIndex]?.key == key &&
+                this.map[hashIndex]?.deleted != true
+            ) {
+                return this.map[hashIndex]?.value
+            } else if (this.map[hashIndex] != null) {
+                break
+            }
+            hashIndex = (hashIndex + magicConstant) % this.capacity
+            magicConstant++
+        }
+        return null
+    }
+
+    fun remove(key: T) {
+        var hashIndex = calculateHash(key)
+        var magicConstant = 1
+        while (this.map[hashIndex] != null) {
+            if (this.map[hashIndex]!!.key == key) {
+                this.map[hashIndex]!!.deleted = true
+                break
+            } else if (this.map[hashIndex] != null) {
+                break
+            }
+            hashIndex = (hashIndex + magicConstant) % this.capacity
+            magicConstant++
+        }
+        this.size--
+    }
+
+    private fun calculateHash(key: T) : Int {
+        return (this.capacity.toDouble() * ((key.hashCode().toDouble() * 0.6180339887) % 1)).toInt()
+    }
 }
